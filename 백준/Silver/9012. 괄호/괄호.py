cf0bba1d3fc54e5ecf95b5c58d9d9ba88f24d00b
suyeon <130628673+suyeon0121@users.noreply.github.com>
2025-03-27 21:17:54 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,20 @@
+t = int(input())
+
+for i in range(t):
+    stack = []
+    a = input()
+    
+    for j in a:
+        if j == '(':
+            stack.append(j)
+        elif j == ')':
+            if stack:
+                stack.pop()
+            else:
+                print("NO")
+                break
+    else:
+        if not stack:
+            print("YES")
+        else:
+            print("NO")
