f684821f1a4d81d1c5e9cc86fea7bfb9e00b4f72
suyeon <130628673+suyeon0121@users.noreply.github.com>
2025-04-12 22:03:15 +0900

    [Gold II] Title: 후위 표기식, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,26 @@
+n = input()
+stack = []
+result = ''
+
+for i in n:
+    if i.isalpha():
+        result += i
+    else:
+        if i == '(':
+            stack.append(i)
+        elif i == '*' or i == '/':
+            while stack and (stack[-1]=='*' or stack[-1]=='/'):
+                result += stack.pop()
+            stack.append(i)
+        elif i == '+' or i == '-':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.append(i)
+        elif i == ')':
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.pop()
+while stack:
+    result += stack.pop()
+
+print(result)
