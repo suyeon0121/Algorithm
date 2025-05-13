b37ac8fd4a7d3ef76675d3e1aac4768273126cbc
suyeon <130628673+suyeon0121@users.noreply.github.com>
2025-05-13 18:20:10 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 2, Time: 2304 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/23882. 알고리즘 수업 － 선택 정렬 2/알고리즘 수업 － 선택 정렬 2.py
@@ -0,0 +1,26 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+cnt = 0
+answer = -1
+
+def selection(arr):
+    global cnt, answer
+    
+    for i in range(n-1, 0, -1):
+        max_item, idx = arr[0], 0
+        for j in range(1, i+1):
+            if arr[j] > max_item:
+                max_item, idx = arr[j], j
+        
+        if arr[i] != arr[idx]:
+            arr[i], arr[idx] = arr[idx], arr[i]
+            cnt += 1
+            
+            if cnt == k:
+                answer = f'{' '.join(map(str, arr))}'
+    
+    return answer
+
+print(selection(arr))
