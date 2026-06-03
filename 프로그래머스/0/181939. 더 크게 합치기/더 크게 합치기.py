03100b56f6135a9897acb8be12e956d471cd84bd
suyeon <130628673+suyeon0121@users.noreply.github.com>
2026-06-03 14:25:32 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,8 +1,2 @@
 def solution(a, b):
-    ab =  str(a) + str(b)
-    ba = str(b) + str(a)
-    
-    if int(ab) >= int(ba):
-        return int(ab)
-    else:
-        return int(ba)
+    return int(max(f'{a}{b}', f'{b}{a}'))
