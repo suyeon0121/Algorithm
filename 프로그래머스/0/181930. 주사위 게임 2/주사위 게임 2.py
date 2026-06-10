99d2b0fc7be07f4d6dbb076a90e3c414dcd833b3
suyeon <130628673+suyeon0121@users.noreply.github.com>
2026-06-10 23:17:49 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,8 +1,8 @@
 def solution(a, b, c):
-    answer = a + b + c
-    if a == b == c:
-        return answer * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
-    elif a != b and a != c and c != b:
-        return answer
+    check = len(set([a, b, c]))
+    if check == 1:
+        return 3*a * 3*(a**2) * 3*(a**3)
+    elif check == 2:
+        return (a + b + c) * (a**2 + b**2 + c**2)
     else:
-        return answer * (a**2 + b**2 + c**2)
+        return a + b + c
